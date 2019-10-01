99cf2a381a82ddbbc6fd7c79469c6c3e0c0aa16d
ci-build <rosswilsonblair+smriprep-circleci@gmail.com>
2019-10-01 18:04:44 +0000

    [skip ci] Updates

--- api/smriprep-workflows-norm-2.py
+++ api/smriprep-workflows-norm-2.py
@@ -2,5 +2,5 @@ from smriprep.workflows.norm import init_anat_norm_wf
 wf = init_anat_norm_wf(
     debug=False,
     omp_nthreads=1,
-    template_list=['MNI152NLin2009cAsym', 'MNI152NLin6Asym'],
+    templates=[('MNI152NLin2009cAsym', {}), ('MNI152NLin6Asym', {})],
 )
