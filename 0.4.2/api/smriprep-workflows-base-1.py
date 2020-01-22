82bcd15d72470b26b07aebb7c874028c71124b89
ci-build <rosswilsonblair+smriprep-circleci@gmail.com>
2020-01-22 23:56:04 +0000

    doc(update) [skip ci]

---
+++ 0.4.2/api/smriprep-workflows-base-1.py
@@ -0,0 +1,20 @@
+from collections import OrderedDict, namedtuple
+from smriprep.workflows.base import init_single_subject_wf
+BIDSLayout = namedtuple('BIDSLayout', ['root'])
+wf = init_single_subject_wf(
+    debug=False,
+    freesurfer=True,
+    hires=True,
+    layout=BIDSLayout('.'),
+    longitudinal=False,
+    low_mem=False,
+    name='single_subject_wf',
+    omp_nthreads=1,
+    output_dir='.',
+    output_spaces=OrderedDict([('MNI152NLin2009cAsym', {}),
+                               ('fsaverage5', {})]),
+    reportlets_dir='.',
+    skull_strip_fixed_seed=False,
+    skull_strip_template=('OASIS30ANTs', {}),
+    subject_id='test',
+)
